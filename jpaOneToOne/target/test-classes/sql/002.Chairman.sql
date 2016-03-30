6cd964ca5633b4e99b7ed247ae38e18f261b9d88
rahul chandna <chandnarahul@yahoo.com>
2016-03-30 13:34:19 +0530

    removed target sql's

--- jpaOneToOne/target/test-classes/sql/002.Chairman.sql
+++
@@ -1,9 +0,0 @@
-create table chairman(
-	chairmanId BIGINT NOT NULL AUTO_INCREMENT,
-	schoolId BIGINT NOT NULL,
-	name NVARCHAR2(100) NOT NULL,
-	PRIMARY KEY (chairmanId)
-);
-CREATE INDEX chairman_IDX1 on chairman (name);
-CREATE UNIQUE INDEX chairman_IDX2 on chairman (chairmanId, schoolId);
-ALTER TABLE chairman ADD FOREIGN KEY (schoolId) REFERENCES school(schoolId);
